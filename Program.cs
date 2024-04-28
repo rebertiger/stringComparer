256d97ad70a65c95528eab9532626187dc4fa320
Recep Berke KAPLAN <recepberkekaplan@gmail.com>
2024-04-28 12:28:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿string string1;
+string string2;
+
+while (true)
+{
+    while (true)
+    {
+        Console.WriteLine("Enter first string to compare: ");
+        string1 = Console.ReadLine();
+        if (string1 != "" && string1.Length > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Wrong input, try again.");
+        }
+    }
+    
+    while (true)
+    {
+        Console.WriteLine("Enter second string to compare: ");
+        string2 = Console.ReadLine();
+        if (string2 != "" && string2.Length > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Wrong input, try again.");
+        }
+    }
+    
+    bool result = string1.ToLower().Equals(string2.ToLower());
+    Console.Write($"'{string1}' and '{string2}' is ");
+    if (result)
+    {
+        Console.WriteLine("same.");
+    }
+    else
+    {
+        Console.WriteLine("not same.");
+    }
+}
+
